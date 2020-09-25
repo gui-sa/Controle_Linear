90b63707ad6db162c8a2af92d9034dba937226a9
Luiz Renato Rodrigues <lrenato10@hotmail.com>
2020-09-25 10:57:03 -0300

    variaveis auxiliares e velocidade do compensador empregada

--- Trabalho_final/trabalaho_final.py
+++ Trabalho_final/trabalaho_final.py
@@ -15,22 +15,22 @@ from random import uniform
 import matplotlib.pyplot as plt
 import tqdm
 
+#%% Parametros do Sistema
+Lh = 0.32 #metros
+Kh = 2.12829*(10 ** -5)#N/(rad/s)²
+I = 0.0264 #kgm²
+m = 0.3182 # kg
+g = 9.81 #m/s²
+b = 0.006856 #(rad/s)^(-1)
+
 #%% Dinamica do aeropendulo
 
 # Definindo dinamica da planta
 #Esta funcao segue a documentacao da funcao odeint : https://www.youtube.com/watch?v=PfXJWa4TrXY (17-09-2020)
 def din_aeropendulo(y, t, v):#dinamica do aeropendulo y = theta_pp /  t= tempo discreto /v = rotacao do motor
-    theta,theta_p = y #y é uma lista com [theta,theta_p]
-    Lh = 0.32 #metros
-    Kh = 2.12829*(10 ** -5)#N/(rad/s)²
-    I = 0.0264 #kgm²
-    m = 0.3182 # kg
-    g = 9.81 #m/s²
-    b = 0.006856 #(rad/s)^(-1)
-    omega = theta_p #velocidade do aeropendulo
-    omega_p = [ omega , ((Lh*Kh/I)*(v ** 2) - (Lh*m*g/I)*np.sin(theta) - (b/I)*omega) ]
-    return omega_p #omega_p = theta_pp
-
+    thetaf,theta_pf = y #y é uma lista com [theta,theta_p]
+    theta_pf_e_ppf = [ theta_pf , ((Lh*Kh/I)*(v ** 2) - (Lh*m*g/I)*np.sin(thetaf) - (b/I)*theta_pf) ]#aceleracao angular
+    return theta_pf_e_ppf #velocidade angular e aceleracao angular
 
 
 #%%======= Parametros de simulacao
@@ -40,46 +40,59 @@ Tsim = 20 #tempo final
 kend = int(Tsim/Ta) # quantidade de iteracoes
 
 # scopes
-v =np.zeros(kend)  # velocidade de rotacao do motor
-theta = np.zeros(kend)-40*np.pi/180 # Angulo do pendulo com a estrutura [rad]
+#Equilibrio
+theta_eq=np.zeros(kend)+2*np.pi/9 #40 graus
+omega_eq=np.zeros(kend)+np.sqrt((m*g*np.sin(theta_eq))/Kh)
+#velocidades
+omega =np.zeros(kend)  # velocidade de rotacao do motor
+v =np.zeros(kend)  # velocidade de rotacao do motor linearizada
+#angulos
+theta = np.zeros(kend)# Angulo do pendulo com a estrutura [rad]
+phi = theta-theta_eq
 theta_med = np.zeros(kend) # Angulo do pendulo com a estrutura
+phi_med=np.zeros(kend) # Angulo do pendulo linearizado
+
 theta_p = np.zeros(kend) # valocidade de aeropendulo
 theta_pp = np.zeros(kend) # aceleracao do aeropendulo
 erro = np.zeros(kend)  #Erro do sistema
-theta_ref = np.zeros(kend) # Angulacao requerida ou referenciada
-
-theta_ref[:]=-40 #0 [graus]
+phi_ref = np.zeros(kend) # Angulacao requerida ou referenciada linearizada
+theta_ref=np.zeros(kend)#Angulo de referencia
+#Referencias
+theta_ref[:]=0 #0 [rad]
+phi_ref[:]=theta_ref[:]-theta_eq # [rad]
 #Parametros do controlador
 Kp = 500 #Só pra testar mesmo
 
 #%%=======================Loop percorrendo o tempo do experimento
 for k in tqdm.tqdm((range(kend-1))):
 
-    if(Ta*k >= 1):
-        theta_ref[k] = 0 #graus
+    if(Ta*k >= 1):#REFERENCIA
+        theta_ref[k] = 40*np.pi/180#angulo de referencial real [rad] 
+        phi_ref[k] = theta_ref[k]-theta_eq[k] #angulo para o controlador[rad]
     
-    theta_ref[k] = theta_ref[k]*np.pi/180  #converte para radianos
-    
-    erro[k] = theta_ref[k] - theta_med[k]
+    #CONTROLADOR
+    erro[k] = phi_ref[k] - phi_med[k]
     v[k]= Kp*erro[k]
-        
-    v[k] = min(v[k], 375)  # maximo 375 rad/s
-    v[k] = max(v[k], 0)  # minimo 0
     
-    #theta[k] = min(theta[k], 90*np.pi/180)  # maximo 375 rad/s
-    #theta[k] = max(theta[k], 0*np.pi/180)  # minimo 0
+    omega[k]=v[k]+omega_eq[k]#rotacao controlador->linear
     
+    #ATUADOR
+    omega[k] = min(omega[k], 375)  # maximo 375 rad/s
+    omega[k] = max(omega[k], 0)  # minimo 0
     
-    sol = odeint(din_aeropendulo, [theta[k] + 40*np.pi/180, theta_p[k]], [ Ta*k, Ta*(k+1) ], args= (v[k],))
-    theta[k+1]=sol[1,0] - 40*np.pi/180
+    #SISTEMA NAO LINEAR
+    sol = odeint(din_aeropendulo, [theta[k], theta_p[k]], [ Ta*k, Ta*(k+1) ], args= (omega[k],))
+    theta[k+1]=sol[1,0]
+    phi[k+1]=theta[k+1]-theta_eq[k+1]
     theta_p[k+1] = sol[1,1]
     theta_med[k+1] = theta[k+1] #+ uniform(-0.01, 0.01)
+    phi_med[k+1]=theta_med[k+1]-theta_eq[k+1]
 #%%======================Plotando resultado:
 
 plt.figure()
-plt.plot(np.linspace(0, Tsim,kend) , theta*180.0/np.pi+40, lw =2.0 , color="k", label = "Posicao aeropendulo")
+plt.plot(np.linspace(0, Tsim,kend) , theta*180.0/np.pi, lw =2.0 , color="k", label = "Posicao aeropendulo")
 theta_ref[-1]=theta_ref[-2]
-plt.plot(np.linspace(0, Tsim,kend) , theta_ref*180.0/np.pi+40, lw =2.0 , color="r", label = "Referencia-angulo")
+plt.plot(np.linspace(0, Tsim,kend) , theta_ref*180.0/np.pi, lw =2.0 , color="r", label = "Referencia-angulo")
 plt.xlabel("Tempo [s]")
 plt.ylabel("Angulo [graus]")
 plt.title("Simulacao do aeropendulo")
